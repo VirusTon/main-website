f8af374760a2c250a8f7c3ff7dd97b25d303da35
VirusTon <ftwmh7598@gmail.com>
2025-08-03 16:18:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+async function login() {
+  const userID = document.getElementById("userID").value;
+  const password = document.getElementById("password").value;
 
+  const response = await fetch('https://script.google.com/macros/s/AKfycbwsfve3uGNbKhZM15VdakGcbmMLwOyZ3CrUQ3aKpzRjM_FRFVfb9NbPrR524dpITBhN/exec', {
+    method: 'POST',
+    body: JSON.stringify({
+      action: "login",
+      id: userID,
+      password: password
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const result = await response.json();
+
+  if (result.status === "success") {
+    alert("✅ تم تسجيل الدخول بنجاح");
+    window.location.href = "home.html";
+  } else {
+    alert("❌ البيانات غير صحيحة");
+  }
+}
